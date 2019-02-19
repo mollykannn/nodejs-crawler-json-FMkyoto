c3a8517cadd44702abc7bd833cf56445d934feea
Molly Kan <molly.kan@evergreensystemltd.com>
2019-02-19 21:26:05 +0800

    Fix some error

--- index.js
+++ index.js
@@ -23,9 +23,6 @@ array.forEach(element => {
       console.log(err)
     } else {
       console.log("Date: " + s_date)
-      if(s_date == "2019-02-04") {
-        endTime = "23:00:00";
-      }
       var startDate = new Date(s_date + "T" + startTime + "Z");
       var endDate = new Date(s_date + "T" + endTime + "Z");
       var resultProductData = JSON.parse(res.text).filter(function (a) {
@@ -33,15 +30,15 @@ array.forEach(element => {
         return date >= startDate && date <= endDate
       });
 
-      fs.readFile('./output/flag-radio.json', function (err, data) {
+      fs.readFile('songList.json', function (err, data) {
         if(typeof data != "undefined") {
           var json = JSON.parse(data)
         } else {
           var json = []
-          fs.appendFile("./output/flag-radio.json",'',(err) => {if (err) throw err;})
+          fs.appendFile("songList.json",'',(err) => {if (err) throw err;})
         }
         json.push(resultProductData)
-        fs.writeFile("./output/flag-radio.json", JSON.stringify(json), (err) => {if (err) throw err;})
+        fs.writeFile("songList.json", JSON.stringify(json), (err) => {if (err) throw err;})
       })
     }
   });
